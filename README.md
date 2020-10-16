7023b93c7b7aed0f1ecf1c120856d6e50f1008f1
Jason Rojas <jason.rojas60037@gmail.com>
2020-10-16 18:03:09 -0400

    Build and test.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## Version
 
-### 0.5.3
+### 0.5.4
 
 ## Philosophy
 This SDK is designed to implement the [EasyEnrollment platform](https://www.easyenrollment.net) into our clients own hosted web-portals. We want to make it fit as seemlessly as possible with the current experience of their sites; because of this, we have provided functionality to add callbacks to the end of each of the necessary flows and we are as unopinionated as possible about the styling of the SDK's flow.
@@ -12,7 +12,9 @@ In the spirit of creating a seemless process we will also be forgoing the verifi
 
 
 ## Change Log
-### v0.5.3 (latest)
+### v0.5.4
+    * Fix bug where single payer employers won't render
+### v0.5.3
     * Add config error page
     * Make state transfer through terms of service widget
 ### v0.5.2
--- assets/react/components/sdk.jsx
+++ assets/react/components/sdk.jsx
@@ -175,6 +175,7 @@ class SDK extends Component {
 
   setStepConfigError = error => {
     this.setState({
+      loading: false,
       step: -1,
       error: error
     });
@@ -182,6 +183,7 @@ class SDK extends Component {
 
   setStep3 = () => {
     this.setState({
+      loading: false,
       step: 3,
       termsOfUse: false,
       streamPayer: null,
@@ -194,6 +196,7 @@ class SDK extends Component {
     if (payer) {
       if (streamPayer && streamPayer.id == payer.id) {
         this.setState({
+          loading: false,
           dependent: dependent ? dependent : this.state.dependent,
           streamPayer: payer ? payer : this.state.streamPayer,
           termsOfUse: false,
@@ -227,6 +230,7 @@ class SDK extends Component {
       }
     } else {
       this.setState({
+        loading: false,
         dependent: dependent ? dependent : this.state.dependent,
         termsOfUse: false,
         step: 4,
@@ -241,18 +245,17 @@ class SDK extends Component {
   restartProcess = () => {
     this.setState(this.defaultState);
     getSDK(this.props).then(({ user, payers, tenant, employer }) => {
-      if (payers.length === 1) {
-        this.setStep4({ payer: payers[0] });
-      } else {
-        this.setStep3();
-      }
       this.setState({
-        loading: false,
         streamUser: user,
         streamPayers: payers,
         streamTenant: tenant,
         streamEmployer: employer
       });
+      if (payers.length === 1) {
+        this.setStep4({ payer: payers[0] });
+      } else {
+        this.setStep3();
+      }
     });
   };
 
@@ -262,7 +265,7 @@ class SDK extends Component {
         streamUser: user,
         streamPayers: payers,
         streamTenant: tenant,
-        streamEmployer: employer,
+        streamEmployer: employer
       });
       if (error) {
         this.setStepConfigError(error);
@@ -272,7 +275,6 @@ class SDK extends Component {
         this.setStep3();
       }
       this.setState({
-        loading: false,
         error: null
       });
     });
