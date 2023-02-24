46c90d16fda8fdcf10b444a9cb6225e91e69604f
JasonMRojas <jasonrojas@LAPTOP-1GVMEH00.localdomain>
2023-02-24 08:54:49 -0500

    SDK Hotfix bump version fix urls

--- assets/shared/requests/terms.js
+++ assets/shared/requests/terms.js
@@ -2,7 +2,7 @@ import { sdkAxios } from '../services/axios';
 
 export const getTerms = async ({ email }) => {
   const termsResponse = await sdkAxios.get(
-    `https://app.tpastream.com.com/sdk-api/terms_of_service`,
+    `https://app.tpastream.com/sdk-api/terms_of_service`,
     {
       params: {
         email: email
@@ -14,7 +14,7 @@ export const getTerms = async ({ email }) => {
 
 export const getTermsText = async ({ email }) => {
   const termsResponse = await sdkAxios.get(
-    `https://app.tpastream.com.com/sdk-api/terms_of_service`,
+    `https://app.tpastream.com/sdk-api/terms_of_service`,
     {
       params: {
         email: email
